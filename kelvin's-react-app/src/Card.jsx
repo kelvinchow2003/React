8a5ca6484353e882d198aefc9515583f0f314654
kelvinchow2003 <kelvinchow2014@gmail.com>
2025-01-01 14:18:01 -0500

    first commit

---
+++ kelvin's-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+import profilePicture from './assets/profile.jpg'
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="" alt="profile picture"></img>
+            <h2>Kelvin Chow</h2>
+            <p>I am a Fourth Year Computer Science Student at Toronto Metropolitan University.</p>
+
+        </div>
+    );
+
+}
+
+export default Card
---
+++ kelvin's-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Kelvin Chow's Website </p>
+        </footer>
+    );
+}
+export default Footer
